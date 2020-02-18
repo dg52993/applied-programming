99f630c1a050f942dbf13456efabcb998678df1e
dg52993 <dana.granadier@kellogg.northwestern.edu>
2020-02-18 03:02:50 +0000

    week7.2

--- dice.rb
+++ dice.rb
@@ -4,9 +4,12 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "<h1>Hello, world</h1>"
+    @die1 = rand(1..6)
+    @die2 = rand(1..6)
+    "<h1> #{@die1} #{@Die2} </h1>"
+    view "dice"
 end
 
-get "/tacos" do 
-    "Yes, please"
-end
+#get "/tacos" do 
+ #   "Yes, please"
+#end
